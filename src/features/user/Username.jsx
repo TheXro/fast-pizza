c8b1344130026c99389342b6f86fccda0cf2dec4
Ravi yadav <ravyyadav97@gmail.com>
2024-07-26 16:41:11 +0530

    added redux

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,8 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Jonas</div>;
+  const username = useSelector((state) => state.user.username); // useSelector is a hook that allows you to extract data from the Redux store state, using a selector function.
+  return <div className="hidden text-sm font-semibold md:block">{username}</div>;
 }
 
 export default Username;
